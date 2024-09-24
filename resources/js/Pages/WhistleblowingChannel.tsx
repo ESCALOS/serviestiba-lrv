13358e008f2db67a19d2c6c79121e072f819bca7
Nanoka <stornblood6969@gmail.com>
2024-09-24 16:14:47 -0500

    Extracto Spinner on component

--- resources/js/Pages/WhistleblowingChannel.tsx
+++ resources/js/Pages/WhistleblowingChannel.tsx
@@ -1,9 +1,38 @@
+import PhotoSwipeVideo from "@/Components/PhotoSwipeVideo";
 import Layout from "@/Layouts/GuestLayout";
 
 function WhistleblowingChannel() {
     return (
         <Layout title="Canal de denuncias">
-            <div>WhistleblowingChannel</div>
+            <div className="px-4 py-12 mx-auto max-w-7xl">
+                <h2 className="text-4xl font-bold text-primary-500">
+                    Canal de denuncias
+                </h2>
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                    <div className="flex flex-col items-center justify-center h-full gap-4 py-8 lg:max-w-lg lg:items-start">
+                        <h1 className="text-xl font-bold">
+                            Para interponer una denuncia referente al Sistema de
+                            Gestión de Prevención de Delitos (ISO 37001)
+                        </h1>
+                        <p className="text-justify">
+                            En Farmex S.A. trabajamos por ofrecer el mejor
+                            servicio, siempre bajo un Código Ético y de
+                            Conducta.
+                        </p>
+                        <p className="text-justify">
+                            Con el objetivo de detectar irregularidades
+                            relacionadas con la compañía hemos creado el Canal
+                            de Denuncias independiente a Farmex S.A.
+                        </p>
+                        <p className="text-justify">
+                            Animamos a empleados, clientes o cualquier persona
+                            que tenga contacto con nuestros servicios a que se
+                            comunique con nosotros y nos haga llegar sus
+                            sugerencias.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 }
--- resources/js/Sections/WorkWithUsForm.tsx
+++ resources/js/Sections/WorkWithUsForm.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FormEvent } from "react";
 import { useForm } from "@inertiajs/react";
 import Swal from "sweetalert2";
+import Spiner from "@/Components/Spiner";
 
 interface WorkWithUsFormValues {
     firstName: string;
@@ -235,27 +236,8 @@ const WorkWithUsForm: React.FC = () => {
                     >
                         {processing ? (
                             <span className="flex items-center justify-center">
-                                <svg
-                                    className="w-5 h-5 mr-2 text-white animate-spin"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <circle
-                                        className="opacity-25"
-                                        cx="12"
-                                        cy="12"
-                                        r="10"
-                                        stroke="currentColor"
-                                        strokeWidth="4"
-                                    ></circle>
-                                    <path
-                                        className="opacity-75"
-                                        fill="currentColor"
-                                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4h-4z"
-                                    ></path>
-                                </svg>
-                                Procesando...
+                                <Spiner />
+                                Enviando...
                             </span>
                         ) : (
                             "Enviar"
